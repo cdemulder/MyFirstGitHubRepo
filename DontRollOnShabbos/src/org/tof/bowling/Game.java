77ef1bbdb5023083c8334c631162753e44f3d5a7
Tof <cdemulder@gmail.com>
2012-07-17 16:59:18 +0200

    Walter Sobchak

--- DontRollOnShabbos/src/org/tof/bowling/Game.java
+++ DontRollOnShabbos/src/org/tof/bowling/Game.java
@@ -6,7 +6,7 @@ public class Game {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("I don't roll on Shabbos");
+		System.out.println("I don't roll on Shabbos -- Walter Sobchak (The Big Lebowski)");
 
 	}
 
