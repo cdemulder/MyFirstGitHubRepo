9652a6337dd143abc6d04e0a7ae7ddcdd48f8f92
Tof <cdemulder@gmail.com>
2012-07-17 16:43:02 +0200

    I don't roll on Shabbos

---
+++ DontRollOnShabbos/src/org/tof/bowling/Game.java
@@ -0,0 +1,13 @@
+package org.tof.bowling;
+
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("I don't roll on Shabbos");
+
+	}
+
+}
